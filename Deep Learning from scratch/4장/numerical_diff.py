11147998b490d2408f131ab73f619081665cc793
jung-ikhyo <dlrgy22@gmail.com>
2020-07-19 16:40:39 +0900

    밑바닥부터 시작하는 딥러닝

---
+++ Deep Learning from scratch/4장/numerical_diff.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def function_1(x):
+    return 0.01 * x ** 2 + 0.1 * x
+
+def numerical_diff(f, x):
+    h = 1e-4
+    return (f(x + h) - f(x - h)) / (2 * h)
+
+x = np.arange(0.0, 20.0, 0.1)
+y = function_1(x)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.plot(x,y)
+plt.show()
+print(numerical_diff(function_1, 5))
