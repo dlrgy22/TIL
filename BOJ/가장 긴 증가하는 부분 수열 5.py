47815c2d6717e0eb63e8f8d5ce068211e8eecbbc
jung-ikhyo <dlrgy22@gmail.com>
2020-08-11 10:08:26 +0900

    BOJ 문제풀이

--- BOJ/가장 긴 증가하는 부분 수열 5.py
+++ BOJ/가장 긴 증가하는 부분 수열 5.py
@@ -2,14 +2,26 @@ import sys
 n = int(sys.stdin.readline())
 num_list = list(map(int, sys.stdin.readline().split()))
 dp = [num_list[0]]
+save_idx = [0]
+stack = []
 length = 1
 for i in range(1, n):
     if dp[length - 1] < num_list[i]:
         dp.append(num_list[i])
+        save_idx.append(length)
         length += 1
     else:
         for idx in range(len(dp)):
             if dp[idx] >= num_list[i]:
                 dp[idx] = num_list[i]
+                save_idx.append(idx)
                 break
 print(length)
+
+for i in range(n - 1, -1, -1):
+    if save_idx[i] == length - 1:
+        stack.append(num_list[i])
+        length -= 1
+
+for i in range(len(stack) - 1, - 1, -1):
+    print(stack[i], end = ' ')
