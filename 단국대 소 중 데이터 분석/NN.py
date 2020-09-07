183609fa741ac974bf03217b0f19ea95681d6888
dlrgy22 <dlrgy22@gmail.com>
2020-09-07 21:37:32 +0900

    단국대 소 중 데이터 분석

--- 단국대 소 중 데이터 분석/NN.py
+++ 단국대 소 중 데이터 분석/NN.py
@@ -43,41 +43,39 @@ classification_one_hot = tf.one_hot(classification, nb_classes)
 classification_one_hot = tf.reshape(classification_one_hot, [-1, nb_classes])
 
 keep_prob = tf.placeholder(tf.float32)
-
-size = 512
-W1 = tf.get_variable("W1", shape=[18, size], initializer=tf.contrib.layers.xavier_initializer())
+size = 54
+W1 = tf.get_variable("W1", shape=[18, size], initializer=tf.contrib.layers.variance_scaling_initializer())
 b1 = tf.Variable(tf.random_normal([size]))
-L1 = tf.nn.relu(tf.matmul(celestial, W1) + b1)
+L1 = tf.nn.leaky_relu(tf.matmul(celestial, W1) + b1)
 L1 = tf.nn.dropout(L1, keep_prob=keep_prob)
 
-W2 = tf.get_variable("W2", shape=[size, size], initializer=tf.contrib.layers.xavier_initializer())
+W2 = tf.get_variable("W2", shape=[size, size], initializer=tf.contrib.layers.variance_scaling_initializer())
 b2 = tf.Variable(tf.random_normal([size]))
-L2 = tf.nn.relu(tf.matmul(L1, W2) + b2)
+L2 = tf.nn.leaky_relu(tf.matmul(L1, W2) + b2)
 L2 = tf.nn.dropout(L2, keep_prob=keep_prob)
 
-W3 = tf.get_variable("W3", shape=[size, size], initializer=tf.contrib.layers.xavier_initializer())
-b3 = tf.Variable(tf.random_normal([size]))
-L3 = tf.nn.relu(tf.matmul(L2, W3) + b3)
-L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
+# W3 = tf.get_variable("W3", shape=[18 * 15, 18 * 15], initializer=tf.contrib.layers.xavier_initializer())
+# b3 = tf.Variable(tf.random_normal([18 * 15]))
+# L3 = tf.nn.relu(tf.matmul(L2, W3) + b3)
+# L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
+#
+# W4 = tf.get_variable("W4", shape=[18 * 15, 18 * 15], initializer=tf.contrib.layers.xavier_initializer())
+# b4 = tf.Variable(tf.random_normal([18 * 15]))
+# L4 = tf.nn.relu(tf.matmul(L3, W4) + b4)
+# L4 = tf.nn.dropout(L4, keep_prob=keep_prob)
 
-W4 = tf.get_variable("W4", shape=[size, size], initializer=tf.contrib.layers.xavier_initializer())
-b4 = tf.Variable(tf.random_normal([size]))
-L4 = tf.nn.relu(tf.matmul(L3, W4) + b4)
-L4 = tf.nn.dropout(L4, keep_prob=keep_prob)
-
-W5 = tf.get_variable("W5", shape=[size, 3], initializer=tf.contrib.layers.xavier_initializer())
+W5 = tf.get_variable("W5", shape=[size, 3], initializer=tf.contrib.layers.variance_scaling_initializer())
 b5 = tf.Variable(tf.random_normal([3]))
-H = tf.matmul(L4, W5) + b5
+H = tf.matmul(L2, W5) + b5
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=H, labels=classification_one_hot))
 #optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
-optimizer = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(cost)
+optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
 prediction = tf.argmax(H, 1)
-
 correct_prediction = tf.equal(tf.argmax(H, 1), tf.argmax(classification_one_hot, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-epochs = 40
-batch_size = 500
+epochs = 2000
+batch_size = 100
 total_batch = int(len(celestial_data) / batch_size)
 shuffle = True
 test_cel = (test_cel - mean) / std
